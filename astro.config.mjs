e567e9099005171b5c5a9d738d18d420873241fe
Andre Bellmann <andre.bellmann@huk-coburg.de>
2023-07-05 14:49:45 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import proxyMiddleware from './plugins/proxy-middleware.mjs';
 
 // https://astro.build/config
-export default defineConfig({});
+export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
+  integrations: [
+    proxyMiddleware("/shield", {
+      target: "https://hukde-de.web.dmz.tuhuk.de",
+      changeOrigin: true,
+    }),
+  ],
+});
