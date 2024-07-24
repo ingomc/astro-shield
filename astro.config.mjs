b1a5c58d700f59ecba5c36fcd8ebf2d75744740d
Andre Bellmann <andre.bellmann@huk-coburg.de>
2024-07-24 17:52:11 +0200

    nice

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import proxyMiddleware from './plugins/proxy-middleware.mjs';
+import icon from "astro-icon";
 
-// https://astro.build/config
-export default /** @type {import('astro').AstroUserConfig} */ defineConfig({
-  integrations: [
-    proxyMiddleware("/shield-develop", {
-      target: "https://hukde-de.web.dmz.tuhuk.de",
-      changeOrigin: true,
-    }),
-  ],
+export default /** @type {import('astro').AstroUserConfig} */defineConfig({
+  integrations: [proxyMiddleware("/shield-develop", {
+    target: "https://hukde-de.web.dmz.tuhuk.de",
+    changeOrigin: true
+  }), icon()]
 });
